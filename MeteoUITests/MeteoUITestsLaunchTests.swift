bc31472329b03e43474e6a79f2384f1a16bc5573
aymenbraham <aymenbraham71@gmail.com>
2023-04-20 10:56:29 +0200

    initialization project

---
+++ MeteoUITests/MeteoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MeteoUITestsLaunchTests.swift
+//  MeteoUITests
+//
+//  Created by aymen braham on 19/04/2023.
+//
+
+import XCTest
+
+class MeteoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
